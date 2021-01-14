ca4c64294cde05fefe1fdaa7a0098b9d2be9be02
Gal Ben Simchon <galbensimchon@gmail.com>
2021-01-14 20:35:01 +0200

    Add routing to frontend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
+import { Fragment } from 'react';
 import { ToastContainer } from 'react-toastify';
+import { Redirect, Route, Switch } from 'react-router-dom';
+
+import NotFound from './components/notFound';
+
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-import QuestionForm from './components/questionForm';
 
 function App() {
   return (
-    <>
+    <Fragment>
       <ToastContainer />
-      <main className="container"></main>
-    </>
+      <main className="container">
+        <Switch>
+          <Route path="not-found" component={NotFound} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </Fragment>
   );
 }
 
