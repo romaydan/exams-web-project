579fdb235a60fe90a316b0fdd4d601d00a9e3ab5
GitHub User Name <rom644@gmail.com>
2021-01-15 16:26:08 +0200

    Update routes App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,14 @@ import Questions from './questions/pages/Questions';
 
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-
+import NewExam from './exams/pages/NewExam/NewExam.jsx'
 function App() {
   return (
     <Fragment>
       <ToastContainer />
       <main className="container">
         <Switch>
+          <Route path="/exams/new" component={NewExam} />
           <Route path="/questions" component={Questions} />
           <Redirect from="/" exact to="/questions" />
         </Switch>
