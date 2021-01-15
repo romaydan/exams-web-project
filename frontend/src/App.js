4adf9a77283e8c48ab9e9ccbc24591ebed9fd71f
GitHub User Name <rom644@gmail.com>
2021-01-15 02:39:18 +0200

    Add NewExam To routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Questions from './questions/pages/Questions';
+import NewExam from './exams/pages/NewExam/NewExam';
 
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
       <main className="container">
         <Switch>
           <Route path="/questions" component={Questions} />
+          <Route path="/exams/new" component={NewExam} />
           <Redirect from="/" exact to="/questions" />
         </Switch>
       </main>
