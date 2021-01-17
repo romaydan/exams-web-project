027125aee2e796478c09dcd67f0bd06894295506
Gal Ben Simchon <galbensimchon@gmail.com>
2021-01-17 19:45:53 +0200

    Fix merging bugs.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 
 import QuestionForm from './questions/components/QuestionForm';
 import Questions from './questions/pages/Questions';
-import NewExam from './exams/pages/NewExam/NewExam';
 import NotFound from './shared/components/NotFound';
+import NewExam from './exams/pages/NewExam/NewExam';
+import Exams from './exams/pages/Exams/Exams';
 
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-import NewExam from './exams/pages/NewExam/NewExam.jsx'
-import Exams from './exams/pages/Exams/Exams';
+
 function App() {
   return (
     <Fragment>
@@ -20,6 +20,7 @@ function App() {
           <Route path="/questions/:id" component={QuestionForm} />
           <Route path="/questions" component={Questions} />
           <Route path="/exams/new" component={NewExam} />
+          <Route path="/exams" component={Exams} />
           <Route path="/not-found" component={NotFound} />
           <Redirect from="/" exact to="/questions" />
           <Redirect to="/not-found" />
--- frontend/src/exams/pages/NewExam/NewExam.jsx
+++ frontend/src/exams/pages/NewExam/NewExam.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import Input from '../../components/common/FormElements/Input'
-import classes from './NewExam.modules.css'
-import Input from '../../../shared/components/FormElements/Input';
-import classes from './NewExam.modules.css';
 import ExamForm from '../../components/ExamForm/ExamForm';
 import QuestionPicker from '../../components/QuestionPicker/QuestionPicker';
 import { saveExam } from '../../../shared/services/examService';
@@ -32,54 +28,8 @@ const NewExam = () => {
     }
     console.log('pickedQuestions :>> ', pickedQuestions);
   };
-  const test = [
-      id: 'language',
-      inputType: 'select',
-      options: {
-        english: 0,
-        hebrew: 1,
-      },
-      label: 'Language: ',
-    },
-      id: 'name',
-      inputType: 'text',
-      label: 'Exam Name: ',
-      placeHolder: 'EnterName',
-      maxLength: 200,
-      id: 'header',
-      inputType: 'textarea',
-      label: 'Header:',
-      id: 'successMessage',
-      inputType: 'textarea',
-      label: 'Success Message:',
-      id: 'failMessage',
-      inputType: 'textarea',
-      label: 'Fail Message:',
-      id: 'name',
-      inputType: 'text',
-      label: 'Exam Name: ',
-      placeHolder: 'EnterName',
-      maxLength: 200,
-  ];
-  const form = test.map((formElement) => {
-    return <Input {...formElement} />;
-  });
 
   return (
-   const form =test.map(formElement=>{
-      return  <Input {...formElement} />
-   })
-    
-    return (
-        <div className={classes.NewExam}>
-            <h2>Exam Creation </h2>
-            {form}
-        </div>
-    )
-}
-    <div className={classes.NewExam}>
-      <h2>Exam Creation </h2>
-      {form}
     <div>
       <ExamForm submited={submitHandler} />
       <QuestionPicker questionSelected={questionPickedHandler} />
