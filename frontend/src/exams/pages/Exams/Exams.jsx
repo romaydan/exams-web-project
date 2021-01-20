905d13a89ae6cc37199d49640a2b396870bc88e3
GitHub User Name <rom644@gmail.com>
2021-01-20 11:36:56 +0200

    Exams page update.

--- frontend/src/exams/pages/Exams/Exams.jsx
+++ frontend/src/exams/pages/Exams/Exams.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 import { getExams } from '../../../shared/services/examService';
+
+import classes from './Exams.module.css';
 const Exams = () => {
   const [exams, setExams] = useState([]);
+  const history = useHistory();
   useEffect(() => {
     getExams()
       .then((res) => setExams(res.data))
@@ -9,13 +14,25 @@ const Exams = () => {
         console.log('error!', error);
       });
   }, []);
-  const changeExamHandler = (exam) => {};
+  const editExamHandler = (exam) => {
+    history.push(`/exams/edit/${exam._id}`);
+  };
+  const deleteExamHandler = (exam) => {};
   const examsElms = exams.map((exam) => (
-    <div onClick={() => changeExamHandler(exam)}>
+    <div className={classes.Exam}>
       <h4>{exam.name}</h4>
+      <div>
+        <Button onClick={() => editExamHandler(exam)}>Edit</Button>
+        <Button onClick={() => deleteExamHandler(exam)}>Delete</Button>
+      </div>
     </div>
   ));
-  return <div>{examsElms}</div>;
+  return (
+    <div className={classes.Exams}>
+      <h2>Exams Page</h2>
+      {examsElms}
+    </div>
+  );
 };
 
 export default Exams;
